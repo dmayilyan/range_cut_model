059cb60eff2d2bc630fa38d08f79b48c2fa4a498
Davit Mayilyan <davit.mayilyan@cern.ch>
2024-05-25 11:29:29 +0200

    Add plotting script

---
+++ plot_losses.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Connect to the SQLite database
+conn = sqlite3.connect('log.db')
+QUERY = """select epoch, train_loss, test_loss from LogConfig where run_id = 212016045419449;"""
+
+data = pd.read_sql(QUERY, conn)
+print(data)
+
+plt.plot(data["epoch"], data["train_loss"], label="train_loss")
+plt.plot(data["epoch"], data["test_loss"], label="test_loss")
+plt.grid()
+plt.legend()
+
+plt.savefig("/eos/home-d/davit/train_test.png")
